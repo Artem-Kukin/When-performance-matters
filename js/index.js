fd79514077852306c922767cc38e73948c2e78e9
Artem <a.ku666@vk.com>
2024-12-01 11:52:09 +0200

    mobile menu

--- js/index.js
+++ js/index.js
@@ -1,23 +1,29 @@
-let navBar = document.querySelector(".nav");
+const navBar = document.querySelector(".nav");
+const menuBtn = document.querySelector(".mobile-menu");
+const menu = document.querySelector(".fixed-mob-menu");
+
+menu.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  document.querySelector(".nav__links").classList.toggle("open");
+});
 
 window.onscroll = () => {
   navBar.classList.toggle("sticky", window.scrollY > 50);
 };
 
-// swiper
+//=== swiper ===//
 
 var swiper = new Swiper(".swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
- 
+  slidesPerView: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
